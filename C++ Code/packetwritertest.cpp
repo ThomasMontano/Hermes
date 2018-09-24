1d3df176727eb22bb0af805a6ca9a35d98d612fe
ThomasMontano <montanot@my.erau.edu>
2018-09-24 00:29:36 -0700

    Added functionality for floating points

--- C++ Code/packetwritertest.cpp
+++ C++ Code/packetwritertest.cpp
@@ -22,32 +22,48 @@ int main(void)
   ofstream packet;
   ifstream packet2;
 
-  short int hexnumber;
-  short int testvalues[3] = {170,90,-7};
-  short int input=1;
+  unsigned char hexnumber;
+  signed char negNumber;
+  unsigned char testvalues[3] = {170,90,7};
+  signed char negValues[4]={-12,-56,-123,0};
+  float floats[1] = {10000.6123};
+
+  float floatTest;
+  int input=1;
 
   do{
 
-  cout << "Enter an integer: ";
+  cout << "Enter an integer less then 256: ";
   cin >> input;
 
   //Open file as a binary write file
   packet.open("packet", ios::binary | ios::out);
 
-  packet.write((char*)&input,2); //Write input as 2 bytes to a file
   packet.write((char*)&testvalues,sizeof(testvalues)); //Write testvalues as 6 bytes
+  packet.write((char*)&input,1); //Write input as 2 bytes to a file
+  packet.write((char*)&negValues,sizeof(negValues));
+  packet.write((char*)&floats,sizeof(floats));
 
   packet.close(); //Remember to close file when finished
 
   //Open file as a binary read
   packet2.open("packet", ios::binary | ios::in);
 
-  packet2.read((char*)&hexnumber,2); //Store 2 bytes in hexnumber
-  cout << hexnumber << endl; //Print read value
+  packet2.seekg(3); //move to after the third byte
+  packet2.read((char*)&hexnumber,1); //Store 1 byte in hexnumber
+  printf("\nReading your input unsigned: %d\n",hexnumber); //Print read value
 
-  packet2.seekg(6); //Use seekg() to control the input position
-  packet2.read((char*)&hexnumber,2); //Store 2 bytes in hexnumber
-  cout << hexnumber << endl; //Print read value
+  packet2.seekg(0); //Use seekg() to control the input position
+  packet2.read((char*)&hexnumber,1); //Store 1 byte in hexnumber
+  printf("Reading a positive value into an unsigned variable: %d\n",hexnumber); //Print read value
+
+  packet2.seekg(4);
+  packet2.read((char*)&negNumber,1);
+  printf("Reading a negative value into a signed variable: %d\n",negNumber);
+
+  packet2.seekg(8);
+  packet2.read((char*)&floatTest,4);
+  printf("Reading a float into a float variable %f\n\n",floatTest);
 
   packet2.close();
 
