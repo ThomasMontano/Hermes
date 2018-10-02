0368115ed5b0b5b337007aecf8a2e133bb9d82a9
ThomasMontano <montanot@my.erau.edu>
2018-10-02 15:10:44 -0700

    Changed LO freq to 2.4G

---
+++ GNURadioFiles/plutotransmitter.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: plutotransmitter
+# Generated: Tue Oct  2 15:10:25 2018
+##################################################
+
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import iio
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from grc_gnuradio import blks2 as grc_blks2
+from optparse import OptionParser
+
+
+class plutotransmitter(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "plutotransmitter")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+        self.freq = freq = 2400000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.pluto_sink_0 = iio.pluto_sink('', freq, 3000000, 3000000, 0x8000, False, 10.0, '', True)
+        self.digital_gfsk_mod_0 = digital.gfsk_mod(
+        	samples_per_symbol=2,
+        	sensitivity=1.0,
+        	bt=0.35,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/esft/datafiles/packet', True)
+        self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
+        		samples_per_symbol=2,
+        		bits_per_symbol=1,
+        		preamble='',
+        		access_code='',
+        		pad_for_usrp=True,
+        	),
+        	payload_length=0,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_encoder_0, 0), (self.digital_gfsk_mod_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blks2_packet_encoder_0, 0))
+        self.connect((self.digital_gfsk_mod_0, 0), (self.pluto_sink_0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.pluto_sink_0.set_params(self.freq, 3000000, 3000000, 10.0, '', True)
+
+
+def main(top_block_cls=plutotransmitter, options=None):
+
+    tb = top_block_cls()
+    tb.start()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
