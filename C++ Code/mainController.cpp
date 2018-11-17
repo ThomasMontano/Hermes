ec5ac3f507564e0676706971b821895a3822cb05
ThomasMontano <montanot@my.erau.edu>
2018-11-17 15:27:42 -0700

    Updated main control code formatting and documentation

--- C++ Code/mainController.cpp
+++ C++ Code/mainController.cpp
@@ -10,11 +10,9 @@
 
 using namespace std;
 
-float getFloatData(ifstream& file, string name);
-int getIntData(ifstream& file, string name);
 string getBuffer(int fd);
-//void packetWrite(ofstream& file, struct packet);
 
+//Main data packet structure that holds data elements for a packet
 struct packet
 {
 
@@ -31,57 +29,67 @@ struct packet
 int main()
 {
 
-  string serialBuffer;
-  int fd;
+  string serialBuffer; //String to hold arduino serial output
+  int fd; //File descriptor for serial port
 
+  //Set the system clock to 0 seconds. Time logged is now seconds since startup
   system("sudo date -s '1970-01-01 00:00:00'");
 
+  //Binary packet write file
   ofstream packet;
+
+  //CSV data logging file
   ofstream dataFile("recordedData.csv", ios_base::app);
 
+  //Struct to record individual data elements
   struct packet dataPacket;
 
+  //Headers for the CSV file
   dataFile << "Time(s),Altitude(m),Temperature(C),Pressure(hPa),Latitude,Longitude,Battery Voltage" << endl;
 
   fd = open("/dev/ttymxc3", O_RDWR | O_NONBLOCK | O_NDELAY);
 
+  //Serial port configuration
   struct termios tty;
   memset(&tty, 0, sizeof(tty));
-
   cfsetospeed(&tty, (speed_t)B9600);
   cfsetispeed(&tty, (speed_t)B9600);
-
   cfmakeraw(&tty);
 
+  //Apply port attributes
   tcflush(fd, TCIFLUSH);
   tcsetattr(fd, TCSANOW, &tty);
 
+  //Wait to ensure configuration has been applied
   usleep(1000000);
 
+  //While payload is powered on
   while(1)
   {
 
   //Read in the serial buffer from the Arduino
   serialBuffer = getBuffer(fd);
 
+  //If the serial buffer is not empty
   if(serialBuffer.length()>1)
   {
+    //Print for debugging purposes
     cout << "Serial Buffer: " << serialBuffer << endl;
 
     //Seperate CSV string and populate struct values
     dataPacket.time = time(NULL);
-	dataPacket.temperature = atof((serialBuffer.substr(0,5)).c_str());
+	  dataPacket.temperature = atof((serialBuffer.substr(0,5)).c_str());
 
-	//Print out values for debugging purposes
-	cout << "Time: " << dataPacket.time << endl;
-	cout << "Temperature: " << dataPacket.temperature << endl << endl;
+	  //Print out values for debugging purposes
+	  cout << "Time: " << dataPacket.time << endl;
+	  cout << "Temperature: " << dataPacket.temperature << endl << endl;
 
     //Open packet file
     packet.open("./datafiles/packet", ios::binary | ios::out);
 
-	//Write the data to the CSV file
-	dataFile << dataPacket.time << "," << dataPacket.altitude << "," << dataPacket.temperature << "," << dataPacket.pressure
-  << "," << dataPacket.latitude << "," << dataPacket.longitude << "," << dataPacket.batteryVoltage << endl;
+	  //Write the data to the CSV file
+	  dataFile << dataPacket.time << "," << dataPacket.altitude << "," << dataPacket.temperature << "," << dataPacket.pressure
+    << "," << dataPacket.latitude << "," << dataPacket.longitude << "," << dataPacket.batteryVoltage << endl;
 
     //Write the packet as a binary stream
     packet.write((char*)&dataPacket.time, sizeof(dataPacket.time));
@@ -96,7 +104,7 @@ int main()
 
   }
 
-  }
+}
 
   return 0;
 }
@@ -106,8 +114,10 @@ string getBuffer(int fd)
 
     char buffer[16] = {0};
 
+    //Read the contents of device file /dev/ttymxc3
 		read(fd, &buffer, sizeof(buffer));
 
+    //Cast buffer as a string
 		string dataString(buffer);
 
 	return dataString;
