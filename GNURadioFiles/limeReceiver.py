a165142956f8c67945e7cd9995e733060b86c0ab
EagleSat <montanothomas20@gmail.com>
2019-02-03 16:41:03 -0700

    Update Gnu Radio Files

---
+++ GNURadioFiles/limeReceiver.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: limeReceiver
+# Generated: Sun Feb  3 16:40:49 2019
+##################################################
+
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from grc_gnuradio import blks2 as grc_blks2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+
+
+class limeReceiver(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="limeReceiver")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2e6
+        self.freq = freq = 2400e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=True,
+        	avg_alpha=.1,
+        	title='FFT Plot',
+        	peak_hold=True,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
+        self.osmosdr_source_1 = osmosdr.source( args="numchan=" + str(1) + " " + 'soapy=0,driver=lime' )
+        self.osmosdr_source_1.set_sample_rate(samp_rate)
+        self.osmosdr_source_1.set_center_freq(freq, 0)
+        self.osmosdr_source_1.set_freq_corr(0, 0)
+        self.osmosdr_source_1.set_dc_offset_mode(2, 0)
+        self.osmosdr_source_1.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_1.set_gain_mode(False, 0)
+        self.osmosdr_source_1.set_gain(10, 0)
+        self.osmosdr_source_1.set_if_gain(20, 0)
+        self.osmosdr_source_1.set_bb_gain(20, 0)
+        self.osmosdr_source_1.set_antenna('', 0)
+        self.osmosdr_source_1.set_bandwidth(5000000, 0)
+
+        self.digital_gfsk_demod_0 = digital.gfsk_demod(
+        	samples_per_symbol=2,
+        	sensitivity=1.0,
+        	gain_mu=0.175,
+        	mu=0.5,
+        	omega_relative_limit=0.005,
+        	freq_error=0.0,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/alarm/dataout.txt', True)
+        self.blocks_file_sink_0.set_unbuffered(True)
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
+        		access_code='',
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
+        	),
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.digital_gfsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.digital_gfsk_demod_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.wxgui_fftsink2_0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.osmosdr_source_1.set_sample_rate(self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.wxgui_fftsink2_0.set_baseband_freq(self.freq)
+        self.osmosdr_source_1.set_center_freq(self.freq, 0)
+
+
+def main(top_block_cls=limeReceiver, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
